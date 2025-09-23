a755d49c57d7cd4406378436f88d38a9aba768ae
ale-blanco-dev <aleblancosanchez0@gmail.com>
2025-09-22 21:55:56 -0500

    fix new errors and add a new functionality for download my cv of any format and language

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -115,3 +115,19 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function toggleDropdown() {
+  document.getElementById("cvDropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches('.dropbtn')) {
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    for (let i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
