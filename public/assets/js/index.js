e88a00e8d74b90bdc6c2690bf77b726f93934cfa
ale-blanco-dev <aleblancosanchez0@gmail.com>
2025-09-25 10:59:38 -0500

    delete de function of download my cv for security reasons

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -115,19 +115,3 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
-
-function toggleDropdown() {
-  document.getElementById("cvDropdown").classList.toggle("show");
-}
-
-window.onclick = function (event) {
-  if (!event.target.matches('.dropbtn')) {
-    let dropdowns = document.getElementsByClassName("dropdown-content");
-    for (let i = 0; i < dropdowns.length; i++) {
-      let openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
